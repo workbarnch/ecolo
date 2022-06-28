de88cf3530a06e10a0e638c10a7dca3d22ca614e
Garik_BWT <khachanyan_gh@groupbwt.com>
2022-06-28 14:24:43 +0400

    prod

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
 from parser import main
-import os
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.post('/')
 def index():
     return jsonify(main(request.json))
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from main import app
+import os
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
