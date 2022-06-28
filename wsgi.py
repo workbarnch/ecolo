41578f168e06f48c09779eebc17372ed120df7ce
Garik_BWT <khachanyan_gh@groupbwt.com>
2022-06-28 15:28:13 +0400

    prod

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from main import app
 import os
+
+PORT = 5000
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    print(f'Server started in port {PORT}')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
