f78ef2269540aa56a5ae22e060da6aa5caf53ca0
Garik_BWT <khachanyan_gh@groupbwt.com>
2022-06-28 17:04:30 +0400

    prod

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from parser import main
+import os
 
 app = Flask(__name__)
+
 
 @app.post('/')
 def index():
     return jsonify(main(request.json))
 
 
-
+PORT = 5000
+if __name__ == '__main__':
+    print(f'Server started in port {PORT}')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from main import app
-import os
-
-PORT = 5000
-if __name__ == '__main__':
-    print(f'Server started in port {PORT}')
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
